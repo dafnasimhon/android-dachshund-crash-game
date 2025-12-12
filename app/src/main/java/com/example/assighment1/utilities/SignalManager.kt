8df24f2e24c0d5ae769736f57b720df1d7cfa2f2
Dafna Simhon <Dafnasimhon@gmail.com>
2025-12-12 18:24:57 +0200

    add folders

---
+++ app/src/main/java/com/example/assighment1/utilities/SignalManager.kt
@@ -0,0 +1,78 @@
+package com.example.assighment1.utilities
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.widget.Toast
+import java.lang.ref.WeakReference
+
+class SignalManager private constructor(context: Context) {
+
+    private val contextRef = WeakReference(context.applicationContext)
+
+    enum class ToastLength(val length: Int) {
+        SHORT(Toast.LENGTH_SHORT),
+        LONG(Toast.LENGTH_LONG)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: SignalManager? = null
+
+        fun init(context: Context): SignalManager {
+            return instance ?: synchronized(this) {
+                instance ?: SignalManager(context).also { instance = it }
+            }
+        }
+
+        fun getInstance(): SignalManager {
+            return instance ?: throw IllegalStateException(
+                "SignalManager must be initialized by calling init(context) before use."
+            )
+        }
+    }
+
+    fun toast(text: String, duration: ToastLength = ToastLength.SHORT) {
+        contextRef.get()?.let { context ->
+            Toast
+                .makeText(
+                    context,
+                    text,
+                    duration.length
+                )
+                .show()
+        }
+    }
+
+    fun vibrate() {
+        contextRef.get()?.let { context ->
+            val vibrator: Vibrator =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    val vibratorManager =
+                        context.getSystemService(
+                            Context.VIBRATOR_MANAGER_SERVICE
+                        ) as VibratorManager
+                    vibratorManager.defaultVibrator
+                } else {
+                    @Suppress("DEPRECATION")
+                    context.getSystemService(
+                        Context.VIBRATOR_SERVICE
+                    ) as Vibrator
+                }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val oneShotVibrationEffect =
+                    VibrationEffect.createOneShot(
+                        500,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                vibrator.vibrate(oneShotVibrationEffect)
+            } else {
+                @Suppress("DEPRECATION")
+                vibrator.vibrate(500)
+            }
+        }
+    }
+}
